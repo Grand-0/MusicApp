0e80c3728b1475e292da5a7d35f14fab5b0d4d85
ShevchukDmitriy <fomovichf85@mail.ru>
2020-07-30 17:40:09 +0300

    first commit

---
+++ MusicApp/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        MediaPlayer media = new MediaPlayer();
+        List<string> treakWay = Music.TreaksWay();
+        int treakCount = 0;
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            if(Pause.IsChecked == true)
+            {
+                media.Play();
+            }
+            else
+            {
+                media.Pause();
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<string> treakName = Music.TreakName();
+            for (int i = 0; i < treakName.Count; i++)
+            {
+                Content = treakName[i] + "/n";
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+
+            treakCount++;
+
+            if (treakCount <= Music.TreakCount())
+            {
+                media.Open(new Uri(treakWay[treakCount], UriKind.Absolute));
+            }
+            else
+            {
+                treakCount = 0;
+                media.Open(new Uri(treakWay[treakCount], UriKind.Absolute));
+            }
+
+            media.Play();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+
+            treakCount--;
+
+            if (treakCount <= -1)
+            {
+                treakCount = Music.TreakCount();
+                media.Open(new Uri(treakWay[treakCount], UriKind.Absolute));
+            }
+            else
+            {
+                media.Open(new Uri(treakWay[treakCount], UriKind.Absolute));
+            }
+
+            media.Play();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            VolumeSlider.Maximum = 100;
+            VolumeSlider.Minimum = 0;
+        }
+
+        private void TreakSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TreakSlider.Maximum = media.NaturalDuration.TimeSpan.TotalMinutes;
+            media.Pause();
+            media.Position = TimeSpan.FromMinutes(TreakSlider.Value);
+            media.Play();
+        }
+
+    }
+}
---
+++ MusicApp/Music.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp
+{
+    class Music
+    {
+        public static List<string> TreakName()
+        {
+            List<string> treakName = new List<string>();
+            var dir = new DirectoryInfo(@"D:/Progects/C# PRO/MusicApp/MusicApp/Music");
+            foreach(FileInfo file in dir.GetFiles())
+            {
+                treakName.Add(dir.Name);
+            }
+            return treakName;
+        }
+
+        public static List<string> TreaksWay()
+        {
+            List<string> treaksWay = new List<string>();
+            var dir = new DirectoryInfo(@"D:/Progects/C# PRO/MusicApp/MusicApp/Music");
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                treaksWay.Add(file.FullName);
+            }
+            return treaksWay;
+        }
+
+        public static int TreakCount()
+        {
+            List<string> lid = TreaksWay();
+            int treakCount = lid.Count;
+            return treakCount;
+        }
+
+        public static int TreakVolume()
+        {
+            int volume = 0;
+            return volume;
+        }
+
+        public static int TreakLenght
+        {
+
+        }
+    }
+}
